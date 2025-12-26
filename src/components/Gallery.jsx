c6ff95e934cf57ef97a7132f71d591b0d0f7134c
Mohammed Baba <mohammedbaba1505@gmail.com>
2025-12-26 14:39:32 +0100

    edit the gallerey section

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -28,7 +28,7 @@ export default function GallerySection() {
   }
 
   return (
-    <section id="gallery" className="py-20 bg-black relative" ref={ref}>
+    <section id="gallery" className="py-2 bg-black relative" ref={ref}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
